801e8446d26ab1860ebd62d7567a412e64c11523
Alex <58958500+SpaceWK@users.noreply.github.com>
2023-03-27 22:48:25 +0300

    Added the initial code for mini project Object Equality.

---
+++ Terra - Object Equality/Terra/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace iQuest.Terra
+{
+    public class Country
+    {
+        public string Name { get; }
+
+        public string Capital { get; }
+
+        public Country(string name, string capital)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Capital = capital ?? throw new ArgumentNullException(nameof(capital));
+        }
+    }
+}
